f0a26234f5f8fdfb50f42d2a3e17a7ad32b4e8bc
maple <maple.devstack@gmail.com>
2026-03-08 21:59:48 +0530

    Part 1 exercises 1.3-1.5

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,16 +1,18 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14},
-  ]
- 
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14},
+    ]
+  }
+  
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
@@ -21,7 +23,7 @@ export default App
 const Header = (prop) => {
   return (
     <>
-      <h1>{prop.course}</h1>
+      <h1>{prop.course.name}</h1>
     </>
   )
 }
